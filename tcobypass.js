6d8432466be7a71d89b401b51456f536b265b700
Neil de Carteret <n3dst4@gmail.com>
2012-02-25 18:29:23 +0000

    expanded to fix any twitter.com too

--- tcobypass.js
+++ tcobypass.js
@@ -1,12 +1,28 @@
 /**
  * tcobypass.js
- * fixes links on web.tweetdeck.com to avoid t.co
+ * fixes links on which reference t.co where possible
  */
+var fullAtts = [             // full URL attribute used on... 
+        "data-ultimate-url", // twitter.com
+        "data-expanded-url", // twitter.com
+        "data-full-url"      // tweetdeck.com
+    ],
+    hrefRegex = /^http:\/\/t\.co\//i,
+    tagNameRegex = /^a$/i,
+    i;
+
 document.addEventListener("mouseover", function (event) {
-    if ((event.target.tagName === "a" || event.target.tagName === "A") &&
-            /\burl-ext\b/.test(event.target.className)) {
-        event.target.setAttribute("href", event.target.getAttribute("data-full-url"));
+    if (tagNameRegex.test(event.target.tagName) &&
+            hrefRegex.test(event.target.getAttribute("href"))) {
+        for (i=0; i < fullAtts.length; i++) {
+            if (event.target.hasAttribute(fullAtts[i])) {
+                event.target.setAttribute(
+                    "href",
+                    event.target.getAttribute(fullAtts[i])
+                );
+                break;
+            }            
+        }
     }
  });
-
 
