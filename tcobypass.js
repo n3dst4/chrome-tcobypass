e411a2879c97fc4c30b66ec6e3b01e7cc65a12e8
Neil de Carteret <n3dst4@gmail.com>
2012-02-25 18:04:16 +0000

    remove stupid jquery dependency

--- tcobypass.js
+++ tcobypass.js
@@ -1,3 +1,12 @@
-$("body").delegate("a.url-ext", "mouseenter", function () {
-    $(this).attr("href", $(this).attr("data-full-url"));
-});
+/**
+ * tcobypass.js
+ * fixes links on web.tweetdeck.com to avoid t.co
+ */
+document.addEventListener("mouseover", function (event) {
+    if ((event.target.tagName === "a" || event.target.tagName === "A") &&
+            /\burl-ext\b/.test(event.target.className)) {
+        event.target.setAttribute("href", event.target.getAttribute("data-full-url"));
+    }
+ });
+
+
